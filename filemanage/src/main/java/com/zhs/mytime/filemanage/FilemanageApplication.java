6790dcba47857d39c24f05e5f06d4eb5420e3dbf
sen <09060542hongsen@sina.com>
2017-10-21 18:42:06 +0800

    mybatis相关

--- filemanage/src/main/java/com/zhs/mytime/filemanage/FilemanageApplication.java
+++ filemanage/src/main/java/com/zhs/mytime/filemanage/FilemanageApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 @SpringBootApplication
-@MapperScan("com.zhs.mytime")
+@MapperScan("com.zhs.mytime.filemanage.dao")
 @EnableConfigurationProperties
 public class FilemanageApplication {
 
