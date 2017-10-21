5ff32afc6421d6bfe319d3be14bb4912d21e0334
sen <09060542hongsen@sina.com>
2017-10-21 21:26:08 +0800

    文件上传

--- filemanage/src/main/java/com/zhs/mytime/filemanage/FilemanageApplication.java
+++ filemanage/src/main/java/com/zhs/mytime/filemanage/FilemanageApplication.java
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
+@EnableTransactionManagement 
 @MapperScan("com.zhs.mytime.filemanage.dao")
 @EnableConfigurationProperties
 public class FilemanageApplication {
--- filemanage/src/main/java/com/zhs/mytime/filemanage/service/ResourceMetadataMapperService.java
+++ filemanage/src/main/java/com/zhs/mytime/filemanage/service/ResourceMetadataMapperService.java
@@ -9,25 +9,29 @@ import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zhs.mytime.filemanage.dao.ResourceMetadataMapper;
 import com.zhs.mytime.filemanage.model.ResourceMetadata;
 
-
+@Service
 public class ResourceMetadataMapperService {
 	
 
 	@Autowired  ResourceMetadataMapper dao;
 
-	
+	@Transactional
 	public int deleteByPrimaryKey(String id){
 		return dao.deleteByPrimaryKey(id);
 	}
 
+	@Transactional
 	public int insert(ResourceMetadata record){
 		return dao.insert(record);
 	}
 
+	@Transactional
 	public int insertSelective(ResourceMetadata record){
 		return dao.insertSelective(record);
 	}
@@ -36,10 +40,12 @@ public class ResourceMetadataMapperService {
 		return dao.selectByPrimaryKey(id);
 	}
 
+	@Transactional
 	public int updateByPrimaryKeySelective(ResourceMetadata record){
 		return dao.updateByPrimaryKeySelective(record);
 	}
 
+	@Transactional
 	public int updateByPrimaryKey(ResourceMetadata record){
 		return dao.updateByPrimaryKey(record);
 	}
